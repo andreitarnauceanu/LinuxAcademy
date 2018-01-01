8874392ab70c907161eb8897d588d793af4a7c1a
Tarnauceanu Andrei <andrei.tarnauceanu@gmail.com>
2018-01-01 22:14:17 +0200

    Initial commit \n Plus first Python_for_SysAdmins exercise.

---
+++ Pyhton_for_SysAdmins/1/variables.py
@@ -0,0 +1,9 @@
+#!/bin/python
+
+first_name = "Andrei"
+last_name = "Tarnauceanu"
+age = 33
+birth_date = "27/11/1984"
+
+print("My name is %s %s." % (first_name, last_name))
+print("I was born on %s and I'm %d years old." % (birth_date, age))
